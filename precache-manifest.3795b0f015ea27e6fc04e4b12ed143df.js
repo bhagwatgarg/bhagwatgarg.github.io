fccfcc5ca969b0edd61581e56fc063cb349f03e2
bhagwatgarg <b.garg2013@gmail.com>
2020-07-18 19:13:48 +0530

    Updates

---
+++ precache-manifest.3795b0f015ea27e6fc04e4b12ed143df.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6830dbd505755fd96a69ef2630e40d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3dbcdca7f1dbc4d59b6",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "15b4a3e697a8740e4534",
+    "url": "/static/css/main.6d6e9d14.chunk.css"
+  },
+  {
+    "revision": "d3dbcdca7f1dbc4d59b6",
+    "url": "/static/js/2.ad78fcde.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.ad78fcde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b4a3e697a8740e4534",
+    "url": "/static/js/main.d1492dd0.chunk.js"
+  },
+  {
+    "revision": "938eb7ce1c61cb74e5cb",
+    "url": "/static/js/runtime-main.587f9003.js"
+  },
+  {
+    "revision": "85eea8a46696bc50e5c488449e3526d2",
+    "url": "/static/media/about-photo.85eea8a4.jpg"
+  },
+  {
+    "revision": "b833202bdbd0ea4f3c01e5470f9375fe",
+    "url": "/static/media/about.b833202b.jpg"
+  },
+  {
+    "revision": "cf83c428092a8f6a260e9704deed9911",
+    "url": "/static/media/achievements-backgroung.cf83c428.jpg"
+  },
+  {
+    "revision": "3f86a471727bb47e78f1141356aac517",
+    "url": "/static/media/connect-background.3f86a471.jpg"
+  },
+  {
+    "revision": "faa2794ae42a37ee565f49b3ecbf2d54",
+    "url": "/static/media/main-background.faa2794a.png"
+  },
+  {
+    "revision": "f897f01661d46822417af04097e0ce06",
+    "url": "/static/media/projects-background.f897f016.jpg"
+  },
+  {
+    "revision": "69c3efeed1a46782d37366619d0cea65",
+    "url": "/static/media/skills-back.69c3efee.webp"
+  },
+  {
+    "revision": "d27152afefc64c6f0aa7eec9da5b5a8c",
+    "url": "/static/media/skills.d27152af.png"
+  }
+]);
