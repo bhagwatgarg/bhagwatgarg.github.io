f7121212c19839fcfe2672531051fecaaf59cf76
Bhagwat Garg <bhagwatgarg10@gmail.com>
2021-01-31 19:19:24 +0530

    added latest projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,8 @@ import "./projects.css";
 import ProjectItem from "./project-item";
 import discussionImage from '../assets/discussion-forum.png';
 import schedulerImage from '../assets/favicon.png';
+import sudokuImage from '../assets/sudoku-solver.png'
+import steganographyImage from '../assets/steganography-tool.png'
 
 const Projects = (props) => {
 	return (
@@ -11,16 +13,22 @@ const Projects = (props) => {
 			<h1 className="section-head">My Projects</h1>
       <div className="main-section-content projects-list">
 				<ProjectItem
+					title="Sudoku Solver"
+					description={"A tool that solves sudokus by extracting sudoku puzzles from the user-provided images"}
+					repo='sudoku-solver'
+					image={sudokuImage}
+				/>
+        <ProjectItem
+					title="Steganography Tool"
+					description={"This tool allows the users to save sensitive information securely."}
+					repo='Steganography-Tool'
+					image={steganographyImage}
+				/>
+				<ProjectItem
 					title="Scheduler"
 					description={"A web app to manage all your calendars at one place"}
 					repo='scheduler-frontend'
 					image={schedulerImage}
-				/>
-        <ProjectItem
-					title="Discussion Forum"
-					description={"A place to carry out a discussion with your friends"}
-					repo='discussion-forum'
-					image={discussionImage}
 				/>
       </div>
 		</section>
